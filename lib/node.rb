ceaa96b0e79cb4f0e04f22c446bce6f9c0178bce
Vido <vidoseaver@gmail.com>
2016-08-27 13:20:55 -0600

    node and node_test done

---
+++ lib/node.rb
@@ -0,0 +1,24 @@
+require "pry"
+class Node
+
+  attr_reader :children, :letter
+
+
+  def initialize(letter = nil)
+    @letter = letter
+    @children = Hash.new
+    @final_letter = false
+  end
+
+  def add_child(node)
+    @children[node.letter] = node
+  end
+
+  def final_letter?
+    @final_letter
+  end
+
+  def final_letter_setter
+    @final_letter = !@final_letter
+  end
+end
